69afa1d98a490752f7be5064f886acc61b0a8944
HellcatOG <pranavunnikrishnan109@gmail.com>
2025-03-24 14:37:47 +0530

    message

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-z$faka4m53p!d9iakp3rsp)p*=)v@(8r2n4j0*3@lc#owv8704
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
